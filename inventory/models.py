f1b0202771d3bb8b380467aae1afc97252ba016f
Stavros Angelopoulos <steveangelopoulos@gmail.com>
2025-05-29 17:05:07 +0300

    Feat: Added the model ('schema')

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=50, blank=True)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    added_on = models.DateTimeField(default=timezone.now)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.quantity} pcs)"
